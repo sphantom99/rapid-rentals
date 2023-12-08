cab845655857b6c0b1c5868fc29bc9da8fef1d31
sphantom99 <georgeal1999@gmail.com>
2023-12-08 12:49:47 +0200

    added user image to avatar

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { auth } from "@/auth";
 import Link from "next/link";
+import Image from "next/image";
 
 async function Avatar() {
   const session = await auth();
@@ -9,12 +10,21 @@ async function Avatar() {
   return (
     <>
       {session ? (
-        <Link
-          href="/api/auth/signout"
-          className=" border-2 rounded-full p-2 border-black hover:bg-yellow-300 hover:text-white hover:border-yellow-400"
-        >
-          Sign out
-        </Link>
+        <div className="flex flex-row gap-2 justify-between items-center align-center">
+          <Image
+            className="rounded-full"
+            alt="image of user"
+            width={30}
+            height={30}
+            src={session?.user?.image || "/wheelIcon.png"}
+          />
+          <Link
+            href="/api/auth/signout"
+            className=" border-2 rounded-lg bg-yellow-300 border-yellow-300 text-white p-1  hover:bg-yellow-400 hover:text-white hover:border-yellow-400"
+          >
+            Sign out
+          </Link>
+        </div>
       ) : (
         <Link href="/api/auth/signin">Sign in</Link>
       )}

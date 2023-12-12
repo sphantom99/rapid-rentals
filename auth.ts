9b0e6465ca5c40f3c8b7106d01702cd3f8c5ef77
sphantom99 <georgeal1999@gmail.com>
2023-12-12 10:08:53 +0200

    added db to nextauth

--- auth.ts
+++ auth.ts
@@ -1,4 +1,38 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
-import Github from "next-auth/providers/github"
-export const { handlers, auth } = NextAuth({ providers: [ Google, Github  ] })
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "./lib/dbConnect";
+import { MongoClient } from "mongodb";
+export const { handlers, auth } = NextAuth({
+  adapter: MongoDBAdapter(clientPromise, {
+    databaseName: "RapidRentals",
+  }),
+  providers: [Google, Github],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      session.user.isAdmin = user.isAdmin;
+      return session;
+    },
+  },
+  events: {
+    createUser: async ({ user }) => {
+      const client = new MongoClient(process.env.MONGO_URI ?? "");
+      try {
+        await client.connect();
+
+        const db = await client.db("RapidRentals");
+        const users = db.collection("users");
+        const res = users.updateOne(
+          { email: user.email },
+          { $set: { isAdmin: false } }
+        );
+      } catch (e) {
+        console.error(e);
+      } finally {
+        client.close();
+      }
+    },
+  },
+});

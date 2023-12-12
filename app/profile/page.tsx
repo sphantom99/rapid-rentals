e84801a7d0729b71ffb45e16ae89b55afbe17c9f
sphantom99 <georgeal1999@gmail.com>
2023-12-12 10:10:43 +0200

    started profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,31 @@
+import { auth } from "@/auth";
+import RentedCarHistory from "@/components/RentedCarHistory";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+async function page() {
+  const session = await auth();
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+  return (
+    <main className=" mt-28 flex  flex-col justify-center align-center items-center gap-8">
+      <Image
+        className="rounded-full"
+        src={session?.user?.image ?? ""}
+        alt="user image"
+        width={180}
+        height={180}
+      />
+      <h1 className="font-bold text-3xl">
+        {session?.user?.name}{" "}
+        {session.user.isAdmin && (
+          <span className="text-gray-300 text-sm">(Admin)</span>
+        )}
+      </h1>
+      <RentedCarHistory />
+    </main>
+  );
+}
+
+export default page;
